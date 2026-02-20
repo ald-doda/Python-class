379e15652dfa66dac95517e96135eda073d2f2c7
aldin <aldinferatovic2@gmail.com>
2026-02-20 21:11:36 +0100

    Add files via upload

---
+++ first lesson/zad1.py
@@ -0,0 +1,5 @@
+#napraviti program u kome se ucitava temperatura u celzijusima i ispisuje se temperatura u farenhajtima
+
+c=float(input("Unesite vrijednost temperature u Celzijusima: ")) #ucitavanje temperature u celzijusima
+f = c * 9/5 + 32 #pretvaranje temperature iz celzijusa u farenhajte koristeci formulu F = C * 9/5 + 32
+print("Temperatura u Farenhajtima je:", f) #ispisivanje temperature u farenhajtima
